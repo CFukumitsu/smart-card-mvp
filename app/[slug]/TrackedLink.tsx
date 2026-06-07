4a1b74e74c8fdcfc04bf4ea11deb48d1590a30cd
César Fukumitsu <Cesar WBS@NB009.dm001.f0.sys.srv.br>
2026-06-06 21:12:50 -0300

    corrige tracked link

--- app/[slug]/TrackedLink.tsx
+++ app/[slug]/TrackedLink.tsx
@@ -1,9 +1,44 @@
-return (
-  <a
-    href={href}
-    target={target}
-    rel={target === "_blank" ? "noopener noreferrer" : undefined}
-    className={className}
-  >
-);
+"use client";
+
+type TrackedLinkProps = {
+  slug: string;
+  eventType: string;
+  href: string;
+  className?: string;
+  children: React.ReactNode;
+  target?: string;
+};
+
+export default function TrackedLink({
+  slug,
+  eventType,
+  href,
+  className,
+  children,
+  target,
+}: TrackedLinkProps) {
+  async function trackClick() {
+    fetch("/api/analytics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        card_slug: slug,
+        event_type: eventType,
+      }),
+    });
+  }
+
+  return (
+    <a
+      href={href}
+      target={target}
+      rel={target === "_blank" ? "noopener noreferrer" : undefined}
+      className={className}
+      onClick={trackClick}
+    >
+      {children}
+    </a>
+  );
 }
